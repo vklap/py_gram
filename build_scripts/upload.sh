46fb515c0ce8d3f2bc6c28d4bbb3335845465f16
Victor Klapholz <victor.klapholz@gmail.com>
2020-08-31 00:12:57 +0300

    remove twine verbose

--- build_scripts/upload.sh
+++ build_scripts/upload.sh
@@ -8,4 +8,4 @@ pip install twine
 echo $'[distutils]\nindex-servers = pypi\n[pypi]' > $PYPI_CONFIG
 echo "username=$PYPI_USERNAME" >> $PYPI_CONFIG
 echo "password=$PYPI_PASSWORD" >> $PYPI_CONFIG
-twine --verbose upload dist/*.tar.gz
+twine upload dist/*.tar.gz
